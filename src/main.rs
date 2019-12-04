331cb5a94297b13b20d03fe5f5caaa0af14a9394
Justin Kur <Justink97@hotmail.com>
2019-12-04 04:14:42 -0500

    Problem 4

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fmt;
 
 use std::fs::File;
 use std::io::prelude::*;
+
 struct Answer {
     p1: String,
     p2: String,
@@ -28,7 +29,7 @@ impl fmt::Display for Answer {
 }
 
 fn main() {
-    let problem = 3;
+    let problem = 4;
     let mut file = File::open(format!("src/inputs/{}.txt", problem)).unwrap();
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
@@ -36,6 +37,7 @@ fn main() {
         1 => p1(&input),
         2 => p2(&input),
         3 => p3(&input),
+        4 => p4(&input),
         _ => unimplemented!(),
     };
     println!("{}", output);
@@ -205,4 +207,74 @@ fn p3(input: &str) -> Answer {
         .unwrap();
     let min_steps = steps.into_iter().min().unwrap();
     Answer::new(min_dist, min_steps)
+}
+
+fn digits(
+    digit: i32,
+    has_rep: bool,
+    last_rep: bool,
+    depth: u8,
+    number: i32,
+    start: i32,
+    end: i32,
+    blacklist: i32,
+    part2: bool,
+) -> i32 {
+    if depth == 6 {
+        if (has_rep || (last_rep && blacklist != digit)) && number >= start && number <= end {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    let mut total = 0;
+    // Only consider digits of equal of greater value
+    for i in digit..10 {
+        let mut update_rep = false;
+        // Blacklist means we've seen 3 or more in a row
+        let update_blacklist = {
+            if last_rep && (digit == i) {
+                digit
+            } else {
+                -1
+            }
+        };
+        if part2 {
+            if last_rep && (digit != i) && (digit != blacklist) {
+                update_rep = true;
+            }
+        } else if digit == i {
+            update_rep = true;
+        }
+        total += digits(
+            i,
+            has_rep || update_rep,
+            digit == i,
+            depth + 1,
+            10 * number + i,
+            start,
+            end,
+            update_blacklist,
+            part2,
+        );
+    }
+    total
+}
+
+fn p4(input: &str) -> Answer {
+    let mut split = input.split("-");
+    let start: i32 = split.next().unwrap().parse().unwrap();
+    let end: i32 = split.next().unwrap().parse().unwrap();
+    let mut sums = Vec::new();
+    for part2 in [false, true].into_iter() {
+        let mut sum = 0;
+        for initial in 1..10 {
+            if initial >= (start / 100000) && initial <= (end / 100000) {
+                sum += digits(initial, false, false, 1, initial, start, end, -1, *part2);
+            }
+        }
+        sums.push(sum);
+    }
+
+    Answer::new(sums[0], sums[1])
 }
