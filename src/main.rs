29f61e0f792c43f0190cf1664866665b290185e7
Justin Kur <Justink97@hotmail.com>
2019-12-10 16:06:29 -0500

    Problem 10

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use advent::*;
 
 fn main() {
-    let problem = 9;
+    let problem = 10;
     let input = get_input(problem);
     let output = problem_multiplex(problem)(&input);
     println!("{}", output);
